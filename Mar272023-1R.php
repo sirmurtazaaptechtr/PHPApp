4836a7c707d27cd573e0986dee217c1a7de783d0
Syed Hussain <sirmurtazaaptechtr@outlook.com>
2023-03-31 17:01:13 +0500

    29-Mar-2023

--- Mar272023-1R.php
+++ Mar272023-1R.php
@@ -1,42 +1,3 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = ""; 
-    $dbname = "pic";
-
-    // Create connection
-    $myConn = mysqli_connect($servername,$username,$password,$dbname);
-    // Check connection
-    if(!$myConn)
-    {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    else
-    {
-        echo "Connected successfully<br>";
-        $sql = "SELECT * FROM `inquiries`";
-        $result = mysqli_query($myConn, $sql);
-        $numOfRows = mysqli_num_rows($result);    
-        echo "<br>$numOfRows<br>";
-
-        if($numOfRows > 0)
-        {
-            echo "<ul>";
-            while($row = mysqli_fetch_assoc($result))
-            {
-                // echo var_dump($row);
-                // echo "<br>";
-                echo "<li>".$row['ID']." | ".$row['FullName']." | ".$row['Email']." | ".$row['Website']." | ".$row['Comment']." | ".$row['Gender']."</li>";
-            }
-            echo "</ul>";
-        }
-        else
-        {
-            echo "<br>No record found.<br>";
-        }
-    }
-    mysqli_close($myConn);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,6 +7,43 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>All Walkins</h1>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = ""; 
+        $dbname = "pic";
+        // Create connection
+        $myConn = mysqli_connect($servername,$username,$password,$dbname);
+        // Check connection
+        if(!$myConn)
+        {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        else
+        {
+            echo "Connected successfully<br>";
+            $sql = "SELECT * FROM `inquiries`";
+            $result = mysqli_query($myConn, $sql);
+            $numOfRows = mysqli_num_rows($result);    
+            echo "<br>$numOfRows<br>";
+            if($numOfRows > 0)
+            {
+                echo "<ul>";
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    // echo var_dump($row);
+                    // echo "<br>";
+                    echo "<li>".$row['ID']." | ".$row['FullName']." | ".$row['Email']." | ".$row['Website']." | ".$row['Comment']." | ".$row['Gender']."</li>";
+                }
+                echo "</ul>";
+            }
+            else
+            {
+                echo "<br>No record found.<br>";
+            }
+        }
+        mysqli_close($myConn);
+    ?>
 </body>
 </html>
